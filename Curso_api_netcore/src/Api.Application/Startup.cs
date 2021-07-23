3512a3f8d5bee1f60efc23a9abf469aa6ca9c80c
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2021-07-23 15:29:42 -0300

    Configurando Swagger

--- Curso_api_netcore/src/Api.Application/Startup.cs
+++ Curso_api_netcore/src/Api.Application/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Api.CrossCutting.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -25,15 +26,28 @@ namespace application
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "application", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Api NetCore 3.1 DDD",
+                    Version = "v1",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("http://netCoreDDD.com.br"),
+                    Contact = new OpenApiContact {
+                        Name = "Amanda Hass",
+                        Email = "amanda@mail.com",
+                        Url = new Uri("http://netCoreDDD.com.br") 
+                    },
+                    License = new OpenApiLicense {
+                        Name = "Termos de Licença de Uso",
+                        Url = new Uri("http://netCoreDDD.com.br") 
+                    }
+                });
             });
-
-            
 
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,9 +56,15 @@ namespace application
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "application v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "application v1");
+                    c.RoutePrefix = string.Empty;
+                }
+            );
 
             app.UseRouting();
 
