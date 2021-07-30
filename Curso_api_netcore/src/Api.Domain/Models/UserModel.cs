00df9e00919ea5035987ef8883dfdbd3d155a8d9
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2021-07-30 14:46:04 -0300

    UserModel e Configuração do AutoMapper

---
+++ Curso_api_netcore/src/Api.Domain/Models/UserModel.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Api.Domain.Models
+{
+    public class UserModel
+    {
+        private Guid _id;
+        public Guid Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private DateTime _createdAt;
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+            set
+            {
+                _createdAt = value == null ? DateTime.UtcNow : value;
+            }
+        }
+
+        private DateTime _updatedAt;
+        public DateTime UpdateAt
+        {
+            get { return _updatedAt; }
+            set { _updatedAt = value; }
+        }
+    }
+}
