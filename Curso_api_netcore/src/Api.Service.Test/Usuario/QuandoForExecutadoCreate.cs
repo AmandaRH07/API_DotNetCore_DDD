778e2646450a5d1e3a763e4ef4263b68835fc7f5
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2021-08-25 13:55:02 -0300

    Testes Service - Get All e Post

---
+++ Curso_api_netcore/src/Api.Service.Test/Usuario/QuandoForExecutadoCreate.cs
@@ -0,0 +1,29 @@
+using Api.Domain.Dtos.User;
+using Api.Domain.Interface.Services.Users;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoCreate : UsuarioTestes
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "Executar método Create")]
+        public async Task E_Possivel_Executar_Metodo_Create()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Post(userDtoCreate)).ReturnsAsync(userDtoCreateResult);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Post(userDtoCreate);
+            Assert.NotNull(result);
+            Assert.Equal(NomeUsuario, result.Name);
+            Assert.Equal(EmailUsuario, result.Email);
+        }
+    }
+}
---
+++ Curso_api_netcore/src/Api.Service.Test/Usuario/QuandoForExecutadoGetAll.cs
@@ -0,0 +1,37 @@
+using Api.Domain.Dtos.User;
+using Api.Domain.Interface.Services.Users;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoGetAll : UsuarioTestes
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "Executar método GETAll")]
+        public async Task E_Possivel_Executar_Metodo_GetAll()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.GetAll()).ReturnsAsync(listaUserDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.GetAll();
+            Assert.NotNull(result);
+            Assert.True(result.Count() >= 5);
+
+            var _listResult = new List<UserDto>();
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.GetAll()).ReturnsAsync(_listResult.AsEnumerable);
+            _service = _serviceMock.Object;
+
+            var _resultEmpty = await _service.GetAll();
+            Assert.Empty(_resultEmpty);
+            Assert.True(_resultEmpty.Count() == 0);
+        }
+    }
+}
