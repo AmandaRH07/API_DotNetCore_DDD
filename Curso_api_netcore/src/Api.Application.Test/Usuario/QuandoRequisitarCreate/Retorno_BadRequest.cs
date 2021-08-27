c8f6a64ab8fa2400891b655e726446249843b16d
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2021-08-27 10:34:29 -0300

    Testes na Application

---
+++ Curso_api_netcore/src/Api.Application.Test/Usuario/QuandoRequisitarCreate/Retorno_BadRequest.cs
@@ -0,0 +1,55 @@
+﻿using Api.Application.Controllers;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interface.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarCreate
+{
+    public class Retorno_Created
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É Possivel Realizar o Created.")]
+
+        public async Task E_Possivel_Invocar_a_Controller_Create()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Post(It.IsAny<UserDtoCreate>())).ReturnsAsync(
+                new UserDtoCreateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns("localhost:5000");
+            _controller.Url = url.Object;
+
+            var userDtoCrate = new UserDtoCreate
+            {
+                Name = nome,
+                Email = email
+            };
+
+            var result = await _controller.Post(userDtoCrate);
+            Assert.True(result is CreatedResult);
+
+            var resultValue = ((CreatedResult)result).Value as UserDtoCreateResult;
+            Assert.NotNull(resultValue);
+            Assert.Equal(userDtoCrate.Name, resultValue.Name);
+            Assert.Equal(userDtoCrate.Email, resultValue.Email);
+        }
+    }
+}
---
+++ Curso_api_netcore/src/Api.Application.Test/Usuario/QuandoRequisitarCreate/Retorno_Created.cs
@@ -0,0 +1,52 @@
+﻿using Api.Application.Controllers;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interface.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarCreate
+{
+    public class Retorno_BadRequest
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É Possivel Realizar o Created.")]
+
+        public async Task E_Possivel_Invocar_a_Controller_Create()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Post(It.IsAny<UserDtoCreate>())).ReturnsAsync(
+                new UserDtoCreateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+            // gerando um problema
+            _controller.ModelState.AddModelError("Name", "É um campo obrigatório");
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns("localhost:5000");
+            _controller.Url = url.Object;
+
+            var userDtoCrate = new UserDtoCreate
+            {
+                Name = nome,
+                Email = email
+            };
+
+            var result = await _controller.Post(userDtoCrate);
+            Assert.True(result is BadRequestObjectResult);
+        }
+    }
+}
